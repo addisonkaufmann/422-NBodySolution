f0872694296892bbd011ed74b822826cedcc5e73
addisonkaufmann <addisonkaufmann@email.arizona.edu>
2017-03-13 12:38:07 -0700

    mains and ignore

---
+++ src/NBodyParallel.java
@@ -0,0 +1,14 @@
+
+public class NBodyParallel {
+	public static void main (String [] args){
+		if (args.length < 4){
+			System.out.println("NBodyParallel numWorkers numBodies bodyRadius numSteps");
+			System.exit(1);
+		}
+		
+		int numWorkers = Integer.parseInt(args[0]);
+		int numBodies = Integer.parseInt(args[1]);
+		int bodyRadius = Integer.parseInt(args[2]);
+		int numSteps = Integer.parseInt(args[3]);
+	}
+}
